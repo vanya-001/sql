8c5cf487c73905cc30e48657f478296159e832ef
Vanya Maheshwari <vanimaheshwari19@gmail.com>
2023-09-08 14:07:29 +0530

    tables added

--- College/Creating DB.sql
+++ College/Creating DB.sql
@@ -1,3 +1,24 @@
 create database college;
 
-use college
+use college;
+
+create table student (
+	id int primary key,
+    name varchar(50),
+    age int not null,
+    Course int
+);
+
+insert into student values(1, "Aman", 21, 11);
+insert into student values(2, "Agam", 21, 12);
+insert into student values(3, "Snigdha", 22, 13);
+insert into student values(4, "Vanya", 20, 11);
+insert into student values(5, "Vidushi", 22, 23);
+
+select * from student;
+
+create table course(
+	Cname varchar(20),
+    Cid int
+);
+drop database college;
